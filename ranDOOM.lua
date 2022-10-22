8e7bc1f69c243f1c1d590726eaa12d7553cf1abe
Алексей Порсев <cheifnon0905@gmail.com>
2022-10-22 19:03:57 +0500

    new2

--- ranDOOM.lua
+++ ranDOOM.lua
@@ -6,6 +6,10 @@ local rDm = {}
 
 local seed = os.time()
 
+local function randomseed(sd)
+    seed = sd
+end
+
 local function random(s, e)
     local x = seed
     x = x ~ (x << 13);
@@ -14,6 +18,17 @@ local function random(s, e)
     seed = x
     x = (x % e) + s
     return x
+end
+
+function rDm.ShuffleTable(t, rand)
+    rand = rand or random
+    local n = #t
+    for i = 1, n do
+        for i = 1, n do
+            local pos = random(1,n)
+            t[pos], t[i] = t[i], t[pos]
+        end
+    end
 end
 
 function rDm.genTable(n)
@@ -31,10 +46,7 @@ function rDm.genTable(n)
         t[i] = i
     end
 
-    for i = 1, n do
-        local pos = random(1,n)
-        t[pos], t[i] = t[i], t[pos]
-    end
+    rDm.ShuffleTable(t, random)
     return t
 end
 
@@ -51,7 +63,12 @@ function rDm.print(t)
     end
 end
 
-function rDm.preRand(t, n, start)
+--[[
+    rDm.makeRand (t : table, n, start : integer )
+    Creates new randomizing function based on table 't',
+    number of indicies 'n' and starting position 'start'
+]]
+function rDm.makeRand(t, n, start)
     if type(n) ~= "number" and math.type(n) ~= "integer" then
         error"rDm.preRand number of table indices should be integer"
     end
@@ -59,17 +76,49 @@ function rDm.preRand(t, n, start)
         error"rDm.preRand starts from integer position"
     end
     local t = t or rDm.genTable(n)
+    local l = #t
     local i = start or 1
     return function() -- rand function generated
+        
+        if i < l then
+            i = i + 1
+        else
+            i = 1    
+        end
         local x = t[i]
-        i = i + 1
         return x
     end
 end
 
-local t = rDm.genTable(50)
+function rDm.saveTable(t, folder)
+    if type(t) ~= "table" then
+        error"rDm.saveTable accepts only tables to save"
+    end
+    if type(folder) ~= "string" then
+        error"rDm.saveTable folder should be a string value"
+    end
+    
+    local f = io.open(folder, "w+")
+    if not f then
+        error"rDm.saveTable error opening file to save"
+    end
+    f:write(string.format("return {\n"))
+    local l = #t
+    local j, m = 1, 1
+    local n = 10
+    
+    for i = 1, l, 1 do
+        f:write(t[i]..", ")
+    end
+    
+    f:write(string.format("\n};"))
+    f:close()
+end
+
+local t = rDm.genTable(15)
 
 rDm.print(t)
-local rand = rDm.preRand(t, 50, 5)
+local rand = rDm.makeRand(t, 15, 5)
+rDm.saveTable(t, "savet.lua")
 print(rand())
 print(rand())
