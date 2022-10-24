a79e7b40ee02bb56c77f3f946027a7dbdc13ba53
Алексей Порсев <cheifnon0905@gmail.com>
2022-10-24 22:04:28 +0500

    Add files via upload

--- ranDOOM.lua
+++ ranDOOM.lua
@@ -9,7 +9,7 @@ local seed = os.time()
 local function randomseed(sd)
     seed = sd
 end
-
+-- actually a xorshift algorithm
 local function random(s, e)
     local x = seed
     x = x ~ (x << 13);
@@ -19,46 +19,61 @@ local function random(s, e)
     x = (x % e) + s
     return x
 end
+local min, max = function()end, function()end
 
+--[[
+    ranDOOM.ShuffleTable(t: table, rand: function(m, n: integer))
+    Shuffles already created ranDOOM table using 'rand' function.
+    The function should be like math.random from Lua stadard library.
+]]
 function rDm.ShuffleTable(t, rand)
     rand = rand or random
-    local n = #t
-    for i = 1, n do
-        for i = 1, n do
-            local pos = random(1,n)
+    local n = t[max]
+    local m = t[min]
+    for i = m, n do
+        for i = m, n do
+            local pos = rand(m,n)
             t[pos], t[i] = t[i], t[pos]
         end
     end
 end
 
-function rDm.genTable(n)
+--[[
+    ranDOOM.genTable (m, n: integer)
+    generates ranDOOM table from minimum ('m') and maximum ('n') values
+]]
+function rDm.genTable(m,n)
     local t = {}
-    if type(n) ~= "number" then
-        if math.type(n) ~= "integer" then
+    if type(n) ~= "number" or type(m) ~= "number" then
+        if math.type(n) ~= "integer" or math.type(m) ~= "integer" then
             error"ranDOOM.genTable accepts only integers!"
         end
     end
-    if n < 0 then
-        error"ranDOOM.genTable accepts only integers bigger than 1!!"
+    if m >= n then
+        error"ranDOOM.genTable 1st value can't be bigger than 2nd!!"
     end
 
-    for i = 1, n do
+    for i = m, n do
         t[i] = i
     end
-
+    t[min] = m
+    t[max] = n
     rDm.ShuffleTable(t, random)
     return t
 end
 
+--[[
+    ranDOOM.print ( t : table )
+    prints the contents of ranDOOM table
+]]
 function rDm.print(t)
     if type(t) ~= "table" then
-        error"rDm.print expected table!!"
+        error"ranDOOOM.print expected table!!"
     end
-    local l = #t
     if l < 2 then
-        error"rDm.print no table values to operate"
+        error"ranDOOOM.print no table values to operate"
     end
-    for i = 1, #t do
+    for i = t[min], t[max] do
         print(i, t[i])        
     end
 end
@@ -68,46 +83,51 @@ end
     Creates new randomizing function based on table 't',
     number of indicies 'n' and starting position 'start'
 ]]
-function rDm.makeRand(t, n, start)
-    if type(n) ~= "number" and math.type(n) ~= "integer" then
-        error"rDm.preRand number of table indices should be integer"
+function rDm.makeRand(t, m, n, start)
+    if type(n) ~= "number" and math.type(n) ~= "integer" or type(m) ~= "number" and math.type(m) ~= "integer" then
+        error"ranDOOM.makeRand number of table indices should be integer"
+    end
+    if m >= n then
+        error"ranDOOM.makeRand 1st value can't be bigger than 2nd!!"
     end
     if type(start) == "number" and math.type(start) ~= "integer" then
-        error"rDm.preRand starts from integer position"
+        error"ranDOOM.makeRand starts from integer position"
     end
-    local t = t or rDm.genTable(n)
-    local l = #t
+    if start > n or start < m then
+        error"ranDOOOM.makeRand can't start from outside of table!!"
+    end
+    local t = t or rDm.genTable(m,n)
     local i = start or 1
     return function() -- rand function generated
         
-        if i < l then
+        if i < t[max] then
             i = i + 1
         else
-            i = 1    
+            i = t[min]
         end
-        local x = t[i]
-        return x
+        return t[i]
     end
 end
 
+--[[
+    ranDOOM.saveTable(t: table, folder: string)
+    Saves ranDOOM table into file at folder.
+]]
 function rDm.saveTable(t, folder)
     if type(t) ~= "table" then
-        error"rDm.saveTable accepts only tables to save"
+        error"ranDOOOM.saveTable accepts only tables to save"
     end
     if type(folder) ~= "string" then
-        error"rDm.saveTable folder should be a string value"
+        error"ranDOOOM.saveTable folder should be a string value"
     end
     
     local f = io.open(folder, "w+")
     if not f then
-        error"rDm.saveTable error opening file to save"
+        error"ranDOOOM.saveTable error opening file to save"
     end
     f:write(string.format("return {\n"))
-    local l = #t
-    local j, m = 1, 1
-    local n = 10
     
-    for i = 1, l, 1 do
+    for i = t[min], t[max], 1 do
         f:write(t[i]..", ")
     end
     
@@ -115,10 +135,4 @@ function rDm.saveTable(t, folder)
     f:close()
 end
 
-local t = rDm.genTable(15)
-
-rDm.print(t)
-local rand = rDm.makeRand(t, 15, 5)
-rDm.saveTable(t, "savet.lua")
-print(rand())
-print(rand())
+return rDn;
